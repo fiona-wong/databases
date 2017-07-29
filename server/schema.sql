a1db13335e15e39d2c2df20da5dec22549955df3
Fiona <feeonawong@gmail.com>
2017-07-29 10:07:02 -0700

    fixed schema upload and added index.js -db

--- server/schema.sql
+++ server/schema.sql
@@ -3,8 +3,26 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(5) NOT NULL AUTO_INCREMENT,
+  text VARCHAR(140) NOT NULL,
+  username INT(5) NOT NULL REFERENCES users(id),
+  -- room INT(5) REFERENCES rooms(id),
+  PRIMARY KEY (id)
+
 );
+
+CREATE TABLE users (
+  id INT(5) NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- CREATE TABLE rooms (
+--   id INT(5) NOT NULL,
+--   roomname VARCHAR(20) NOT NULL,
+--   PRIMARY KEY (id)
+  
+-- );
 
 /* Create other tables and define schemas for them here! */
 
